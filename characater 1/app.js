3f41644734f7ceb16207c1ea41e638e2f4027369
Liem <ngotrongliem2004@gmail.com>
2025-01-12 20:41:51 +0700

    mã khởi tạo server

--- characater 1/app.js
+++ characater 1/app.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+const { createServer } = require('http'); 
+
+const hostname = '127.0.0.1'; // localhost
+const port = 6969;
+
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n Liem danng hoc node js !');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
