705ec98337a099c260ef1f6b008fdb53699de56b
Liem <ngotrongliem2004@gmail.com>
2025-01-13 21:23:00 +0700

    template ejs va file env

---
+++ EJS/src/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 8888;
+const hostname = 'localhost';
+
+// Config template engine
+app.set('views', path.join(__dirname, 'views') ); // Đường dẫn đến thư mục views
+app.set('view engine', 'ejs'); // Đúng cú pháp: view engine
+
+// Route
+app.get('/', (req, res) => {
+  res.render('sample'); 
+});
+
+// Start server
+app.listen(port,hostname, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
--- characater 1/app.js
+++ characater 1/app.js
@@ -8,6 +8,13 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+app.get('/abc', (req, res) => {
+  res.send('abc page');
+});
+
+app.get('/ngotrongliem', (req, res) => {
+  res.send('</h1>ngotrongliem</h1>');
+});
 
 app.listen(port, () =>{
   console.log(`Example app listening at http://localhost:${port}`);
