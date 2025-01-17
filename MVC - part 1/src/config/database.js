34a13834da6d43a05fce4c04545c57988b34a135
Liem <ngotrongliem2004@gmail.com>
2025-01-17 22:22:31 +0700

    connect pool db

--- MVC - part 1/src/config/database.js
+++ MVC - part 1/src/config/database.js
@@ -1,12 +1,22 @@
 
 const mysql = require('mysql2');
 
-const Connection = mysql.createConnection({
+// const Connection = mysql.createConnection({
+//     host: process.env.DB_HOST || 'localhost',
+//     port: process.env.DB_PORT || '3307',
+//     user: process.env.DB_USER || 'root',
+//     password: process.env.DB_PASSWORD || '',
+//     database: process.env.DB_NAME || 'test',
+// });
+const Connection = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || '3307',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'test',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
 });
 
 module.exports = Connection;
