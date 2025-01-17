40609953dfa09a67c098f9f287de6f1cbdf11d68
Liem <ngotrongliem2004@gmail.com>
2025-01-17 21:58:43 +0700

    kết nối với mýql

--- MVC - part 1/src/app.js
+++ MVC - part 1/src/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const configViewEngine = require('./config/viewEngine');
+const mysql = require('mysql2');
 const app = express();
 const port = process.env.PORT || 8888;
 const hostname = 'localhost';
@@ -13,6 +14,22 @@ configViewEngine(app);
 app.use('/', webRouter);
 app.use('/about', webRouter);
 
+// test Connection to Database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3307,
+  user: 'root',
+  password: '123456',
+  database: 'hoidanit',
+});
+
+connection.query(
+  'SELECT * FROM Users u',
+  function(err, results, fields) {
+    console.log(">>> results: " , results); // results contains rows returned by server
+    console.log(">>> fields: " ,fields); // fields contains extra meta data about results, if available
+  }
+)
 // Start server
 app.listen(port,hostname, () => {
   console.log(`Example app listening at http://localhost:${port}`);
